0dfa525c46bfd9d1e2b3698d917f22f4e213be81
Nadia <nadeeaulia@gmail.com>
2024-06-27 04:54:34 +0700

    commit

---
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color.fromARGB(255, 214, 245, 194);
+const kShrinePink100 = Color.fromARGB(255, 167, 226, 133);
+const kShrinePink300 = Color.fromARGB(255, 118, 168, 116);
+const kShrinePink400 = Color.fromARGB(255, 81, 121, 93);
+
+const kShrineBrown900 = Color.fromARGB(255, 37, 58, 42);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
